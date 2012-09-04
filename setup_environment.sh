c2404fce839e851980d62a0a3f6380aec174f05d
Will Trimble <trimble@anl.gov>
2012-09-04 17:41:14 -0500

    add setup_environment script

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-# This script prepares data and dependencies for 
+# This script prepares data and dependencies for wltrimbl/genecall_test
 
+sudo apt-get install git
+
+mkdir ~/bin
+
+mkdir ~/build
+cd ~/build
+
+git clone https://github.com/wltrimbl/genecall_test.git
+
+wget http://www.mcs.anl.gov/~trimble/abinitio/fixedlength-bygenome-byreadingframe.zip 
