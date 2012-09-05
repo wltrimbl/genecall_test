aef499e9e79b9e8393b683f98c24a7d7700ec44f
Will Trimble <trimble@anl.gov>
2012-09-05 11:07:59 -0500

    Added first few aa-counting scripts

--- fastalengths.pl
+++ fastalengths.pl
@@ -1,17 +1,27 @@
 #!/usr/bin/perl -w
 # one line at a time fasta parser parse fasta
 use Getopt::Long;
-my $usage = qq(fastalength.pl [-fasta ] <FILENAME> 
+use strict;
+my $usage = qq(fastalength.pl [-q] [-header] [-ambig] [-gc] [-nolength] <FILENAME> 
 );
 my $fastq = 0;
+my $ambig = 0;
+my $gc = 0;
+my $header = 0;
+my $nolength = 0;
 
 if ( ! GetOptions (
                    "header!"          => \$header,
+                   "ambig!"          => \$ambig,
+                   "nolength!"          => \$nolength,
+                   "gc!"          => \$gc,
                    "q!"          => \$fastq,
                   )
    ) { die $usage; }
 
 # relying on positional argument and <> here...
+if($#ARGV+1 > 1 && ($ARGV[0] =~ m/fq$/ || $ARGV[0] =~ m/fastq$/)) {$fastq=1;}
+
 if(!$fastq)
 {
 $/='>';
@@ -19,13 +29,17 @@ while(<>)
 	{
 	if(length($_) > 1)
 		{
+		my ($fastaheader, $sequencestring, $fastaid, $sequence);
 		if( m/(.*?)\n(.*)/is) { $fastaheader=$1; $sequencestring=$2;}
 		chomp;  # removes delimiter
 		if( $fastaheader =~ /(.*?) (.*)/) {$fastaid = $1;} else {$fastaid = $fastaheader;}
 		$sequencestring =~ s/>$//i;  # remove trailing >
 		$sequence = $sequencestring; $sequence  =~ s/\n//isg;
 		if($header){ print "$fastaid\t";}
-	        print length($sequence)."\n";
+		if($gc){ if(length($sequence)>0) {my $x = (s/G/G/g + s/C/C/g)/length($sequence); print "$x\t";}else {print "0\t";}}
+		if($ambig) { my $s = $sequence; my $n= length($sequence) - ($s =~ s/[AaCcGgTt]/N/g) ; print "$n\t";} 
+	        if(!$nolength){print length($sequence);}
+		print "\n";
 		}
 	}
 }
@@ -36,6 +50,7 @@ while(<>)
 	{
 	if(/^@(.*)\n/)
 		{
+		my ($fastaheader, $sequencestring, $fastaid, $sequence);
 		$fastaheader=$1;
 		if( $fastaheader =~ /(.*?) (.*)/) {$fastaid = $1;} else {$fastaid = $fastaheader;}
 		if( m/(.*?)\n(.*)/is) { $fastaheader=$1; $sequencestring=$2;}
@@ -43,7 +58,9 @@ while(<>)
 		$sequence = $sequencestring; $sequence  =~ s/\n//isg;
 		<>; <>; 		
 		if($header){ print "$fastaid\t";}
-	        print length($sequence)."\n";
+		if($ambig) { my $s = $sequence; my $n= length($sequence) - ($s =~ s/[AaCcGgTt]/N/g) ; print "$n\t";} 
+	        if(!$nolength){print length($sequence);}
+		print "\n";
 		}
 	}
 }
