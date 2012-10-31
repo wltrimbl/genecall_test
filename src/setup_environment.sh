9b4cfa66c68024f473b493ca3d41ae655e603dd9
Will Trimble <trimble@anl.gov>
2012-10-31 16:38:52 -0500

    Forgot dependency on FGS

--- src/setup_environment.sh
+++ src/setup_environment.sh
@@ -4,18 +4,28 @@
 sudo apt-get install git -y
 sudo apt-get install zip -y
 mkdir ~/bin
-
 mkdir ~/build
 cd    ~/build
 
+# get genecall test scripts
 git clone https://github.com/wltrimbl/genecall_test.git
 ln -s ~/build/genecall_test/* ~/bin
 
+# install blast
 wget ftp://ftp.ncbi.nih.gov/blast/executables/release/2.2.26/blast-2.2.26-x64-linux.tar.gz
 tar xvf blast-2.2.26-x64-linux.tar.gz
+rm blast-2.2.26-x64-linux.tar.gz
 ln -s $HOME/build/blast-2.2.26/bin/blastall         ~/bin
 ln -s /home/ubuntu/build/blast-2.2.26/bin/formatdb  ~/bin
 
+# get FragGeneScan
+git clone https://github.com/wltrimbl/FGS.git
+cd FGS
+make
+make fgs
+export PATH=$PATH:$HOME/build/FGS
+
+# get labeled test data bundle
 cd /mnt
 mkdir testing
 cd testing
