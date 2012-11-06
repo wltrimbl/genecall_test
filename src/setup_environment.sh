e8ebe16584ac311b9f9f622330664d1c3408f7c0
Will Trimble <trimble@anl.gov>
2012-11-06 09:03:29 -0600

    More fixes to setup

--- src/setup_environment.sh
+++ src/setup_environment.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 # This script prepares data and dependencies for wltrimbl/genecall_test
 
+sudo apt-get update 
 sudo apt-get install git -y
-sudo apt-get install zip -y
+sudo apt-get install zip unzip -y
+sudo apt-get install make gcc -y  # needed to compile FGS
+sudo apt-get install python-matplotlib  -y
 mkdir ~/bin
 mkdir ~/build
 cd    ~/build
+chown ubuntu /mnt
 
 # get genecall test scripts
 git clone https://github.com/wltrimbl/genecall_test.git
 ln -s ~/build/genecall_test/* ~/bin
+export PATH=$PATH:$HOME/build/genecall_test/src
+echo 'export PATH=$PATH:$HOME/build/genecall_test/src' >> ~/.profile
 
 # install blast
 wget ftp://ftp.ncbi.nih.gov/blast/executables/release/2.2.26/blast-2.2.26-x64-linux.tar.gz
@@ -24,6 +30,7 @@ cd FGS
 make
 make fgs
 export PATH=$PATH:$HOME/build/FGS
+echo 'export PATH=$PATH:$HOME/build/FGS' >> ~/.profile
 
 # get labeled test data bundle
 cd /mnt
@@ -35,9 +42,15 @@ unzip fixedlength-bygenome-byreadingframe.zip
 testFGS.pl --input ./ --output testout2 --quicktest
 testFGS.pl --input ./ --output testout2 --quicktest --addup | grep '    ' > testout2/summary.csv
 
-generate-roc.pl /mnt/testing/testout2/BS-0p5-150 
 generate-roc.pl /mnt/testing/testout2/BS-0p5-150  >  /mnt/testing/testout2/BS-0p5-150.ROCinput.csv
-generate-roc.pl /mnt/testing/testout2/A2-0p5-150  >  /mnt/testing/testout2/A2-0p5-150.ROCinput.csv
+
+for i in /mnt/testing/testout2/??-???-*.0.csv 
+do 
+	f=${i/.0.csv/}
+	echo generate-roc.pl $f 
+	generate-roc.pl $f > $f.ROCinput.csv 
+done
+
 roc-plot.py /mnt/testing/testout2/??-0p5-150.ROCinput.csv
 
 annotatemetasim.py  BP-0p2.metasim.fasta BP1.ptt 
